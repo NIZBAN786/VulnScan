bb9a2a715f56fee7fad6ce452532963ed844c03d
Nizban Khoja <julubhai110@gmail.com>
2024-12-23 22:53:40 +0530

    modified:   requirement.txt

---
+++ temp.py
@@ -0,0 +1,20 @@
+import scapy.all as scapy
+
+# scapy.ls(scapy.IP)
+
+# scapy.ls(scapy.TCP)
+
+# scapy.ls(scapy.UDP)
+
+# scapy.ls(scapy.ICMP)
+
+# scapy.ls(scapy.Ether)
+
+# scapy.ls(scapy.ARP)
+
+# def packet_callback(packet):
+#     packet.show()
+
+# Sniff packets on the network
+# scapy.sniff(prn=packet_callback, count=10)
+
